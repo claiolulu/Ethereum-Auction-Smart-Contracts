e9a4875ec27cb8e15bc0c0dca73b7a12043e3647
Claio Liu <claioliu@gmail.com>
2023-12-14 18:50:49 +0000

    dutch Action modification

--- dutchAuc.sol
+++ dutchAuc.sol
@@ -2,47 +2,74 @@
 pragma solidity ^0.8.4;
 contract DutchAuction {
 
-	address payable public immutable beneficiary; 
-	uint public immutable startingPrice;
-    uint public immutable minPrice;
-	uint public startAt;
-	uint public immutable expiresAt;
-	uint public immutable discountRate;
-	uint private immutable duration;
+	address payable public immutable beneficiary; //contract owner address
+	uint public immutable startingPrice; //start price of contract
+    uint public immutable minPrice;  //minimum price of contract
+	uint public immutable startAt;   //timestamp of contract start
+    uint public immutable endAt;    //timestamp of contract end
+	uint public immutable discountRate; //price drop per step
+	uint private immutable duration;  //price holding duration
     
     bool ended;
 	
-
-	constructor(address payable beneficiaryAddress, uint _startingPrice,uint _discountRate, uint endPrice, uint _duration) {
+    //Test construcator value
+    //0x2C89DF082212717B6D5B8BedDEeeBE3fA03D215F
+    //100000000000000000
+    //1000000000000000
+    //10000000000000000
+    //10
+	constructor(address payable beneficiaryAddress, uint _startingPrice,uint _discountRate, uint _minPrice, uint _duration) {
 		beneficiary = beneficiaryAddress;
         startingPrice = _startingPrice;
         startAt = block.timestamp;
-        expiresAt = block.timestamp + _duration;
-        minPrice = endPrice;
+        minPrice = _minPrice;
         discountRate = _discountRate;
+        duration = _duration;
+        //Calculate the end time of the contract
+        endAt = startAt + ((startingPrice - minPrice) / discountRate + 1) * duration;
+        //Validate the start price and minimum price
+        require(_startingPrice >= _minPrice, "starting price < min");
 	}
 
+    //Test function need to remove
+    function getDropTime() public view returns (uint) {
+        uint timeElapsed = block.timestamp - startAt;
+        return timeElapsed / duration;
+    }
+
+    //View function to check the current price without gas fee
     function getPrice() public view returns (uint) {
         uint timeElapsed = block.timestamp - startAt;
-        uint discount = discountRate * timeElapsed;
-        return startingPrice - discount;
+        //Maintain the price for a specific duration, then gradually reduce it in incremental steps
+        uint discount = discountRate * (timeElapsed / duration);
+        //Stop the price drop and set it to minimun price when it reaches the minimum price
+        uint price;
+        if(discount > startingPrice - minPrice){
+            //Constract ended, set the price lower than the minPrice 
+            price = minPrice;
+        }
+        else {
+            price = startingPrice - discount;
+        }
+        return price;
     }
 
 	function buy() external payable{
-		require(block.timestamp < expiresAt, "auction expired");
-
-        uint price = getPrice();
-    
-        require(price >= minPrice, "bid ended");
-
-        require(msg.value >= price, "bid value is lower than the current price");
-        
+        //Validate the contract state before taking any further action
         if(ended)
             revert("bid ended");
 
-        ended=true;
+        //Validate the contract state by comparing the current timestamp and endtime
+        require(block.timestamp >= endAt, "bid ended");
+        
+        uint price = getPrice();
+
+        require(msg.value >= price, "bid value is lower than the current price");
+
+        ended = true;
 
         beneficiary.transfer(msg.value);
+
 	}
 
 }
